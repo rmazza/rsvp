a4d99cc28486c4699f8dd0402d20d379d9742660
Robert Mazza <RobertM@spotcable.com>
2017-07-21 14:00:30 -0500

    added mongodb schemas

--- server/api.js
+++ server/api.js
@@ -7,6 +7,8 @@
 
 const jwt = require('express-jwt');
 const jwks = require('jwks-rsa');
+const Event = require('./models/Event');
+const Rsvp = require('./models/Rsvp');
 
 /*
  |--------------------------------------
